eb293c458df1600d6cceffc81a96e89481551161
Tyler Whitehurst <twwhitehurst@gmail.com>
2023-12-29 23:25:43 +0000

    staging updates

--- lua/tyler/packer.lua
+++ lua/tyler/packer.lua
@@ -14,12 +14,10 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
@@ -33,13 +31,9 @@ return require('packer').startup(function(use)
 	})
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
   use('nvim-treesitter/playground')
-
   use('theprimeagen/harpoon')
-
   use('mbbill/undotree')
-
   use('tpope/vim-fugitive')
 
 	use {
